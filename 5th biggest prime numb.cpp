1628b1641fbc053034fbd3bb08abdcd946e77263
iliyami <iliya.mi78@gmail.com>
2019-11-13 10:56:47 +0330

    Add files via upload

---
+++ 5th biggest prime numb.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,z=0;
+	cin>>n;
+	int i=n-1;
+	
+	for(n;n>1;n--){
+		for(i;i>1;i--){
+			if(n%i==0){
+				i=n-1;
+				break;
+			}
+		}
+		if(i==1){
+			i=n-1;	
+			z++;
+		}
+		if(z==5){
+			break;
+		}
+	}
+	cout<<n;
+	
+	return 0;
+}
